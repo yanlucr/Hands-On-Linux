fc3fced7dfc7d9252f9b3cc01f6d2e54d9d974c5
DarlyssonMelo <darlysson.lima@ufam.edu.br>
2024-10-08 19:40:14 -0400

    Update smartlamp.ino

--- smartlamp.ino
+++ smartlamp.ino
@@ -1,19 +1,27 @@
-int ledPin = 25;        // Pino de saída para o LED
+#include "DHT.h"
+
+int ledPin = 23;        // Pino de saída para o LED
 int ledValue = 10;     // Valor inicial do LED (0 a 100)
 
-int ldrPin = 26;       // Pino de entrada para o LDR
+int ldrPin = 36;       // Pino de entrada para o LDR
 int ldrMax = 4000;     // Valor máximo do LDR encontrado após testes
 
+#define DHT_PIN 25
+#define DHTTYPE DHT11
+
+DHT dht(DHT_PIN, DHTTYPE);
+
+float humidity, temperature;
+String message;
+
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(9600);
     
     pinMode(ledPin, OUTPUT);
     pinMode(ldrPin, INPUT);
 
     // Inicializando o valor do LED com o valor inicial
     ledUpdate();  
-    
-    Serial.printf("SmartLamp Initialized.\n");
 }
 
 // Função loop será executada infinitamente pelo ESP32
@@ -37,9 +45,20 @@ void processCommand(String command) {
         }
     } else if (command.equals("GET_LED")) {
         Serial.printf("RES GET_LED %d\n", ledValue);  // Retorna o valor atual do LED
+        
     } else if (command.equals("GET_LDR")) {
         int ldrValue = ldrGetValue();
         Serial.printf("RES GET_LDR %d\n", ldrValue);  // Retorna o valor atual do LDR
+        
+    } else if (command.equals("GET_TEMP")) {
+        temperature = dht.readTemperature();
+        message = "RES GET_TEMP " + String(temperature, 2);
+        Serial.println(message);  // Retorna o valor atual d
+        
+    }else if (command.equals("GET_HUM")) {
+        humidity = dht.readHumidity();
+        message = "RES GET_HUM " + String(humidity, 2);
+        Serial.println(message);  // Retorna o valor atual d
     } else {
         Serial.printf("ERR Unknown command.\n");  // Comando inválido
     }
